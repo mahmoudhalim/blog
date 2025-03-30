926787cf77691b8f01390049fe61d830d28ebac4
mahmoud isa <mahmoudahalim0@gmail.com>
2025-03-30 21:23:58 +0200

    implemented  mostBlogs and added tests

---
+++ utils/list_helper.js
@@ -0,0 +1,93 @@
+const _ = require('lodash')
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((most, blog) => (blog.likes > most.likes ? blog : most))
+}
+const blogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0,
+  },
+]
+const mostBlogs = (blogs) => {
+  let authors = {}
+  blogs.forEach((blog) => {
+    const author = blog.author
+    if (authors[author]) {
+      authors[author]++
+    } else {
+      authors[author] = 1
+    }
+  })
+  let mostAuthor
+  Object.entries(authors).forEach(([author, blogs]) => {
+    console.log(author, blogs)
+    if (!mostAuthor || blogs > mostAuthor.blogs) {
+      mostAuthor = {
+        author,
+        blogs,
+      }
+    }
+  })
+  return mostAuthor
+}
+
+const mostLikes = (blogs) => {
+  console.assert(false, 'NotImplemented')
+}
+console.log(mostLikes(blogs))
+
+module.exports = {
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+}
